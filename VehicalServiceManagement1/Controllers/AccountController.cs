4fc6cd4d4462ed03714f3748e75287d727926d0b
HP <HP@DESKTOP-O5QV3AC>
2022-08-13 23:44:09 +0530

    Add project files.

---
+++ VehicalServiceManagement1/Controllers/AccountController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VehicalServiceManagement1.Models;
+using System.Web.Security;
+
+namespace VehicalServiceManagement1.Controllers
+{
+ 
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(MemberShip model)
+        {
+            using (var context = new ServiceCenterEntities())
+            {
+                //bool isValid = context.UserLogin.Any(x => x.UserName == model.UserName && x.Password == model.Password);
+                bool isValid = context.UserLogin.Any(x => x.UserName == model.UserName && x.Password == model.Password);
+
+
+
+                //bool isValid = false;
+
+
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("index", "SerCenters");
+                }
+
+
+                ModelState.AddModelError("", "Invalid UserName Or Password");
+                return View();
+
+            }
+            
+            
+        }
+
+        public ActionResult SignUp()
+        {
+
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(UserLogin model)
+        {
+            using (var context = new ServiceCenterEntities())
+            {
+                context.UserLogin.Add(model);
+                context.SaveChanges();
+            }
+
+
+            return RedirectToAction("Login");
+
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+    }
+}
---
+++ VehicalServiceManagement1/Controllers/SerCentersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VehicalServiceManagement1;
+
+namespace VehicalServiceManagement1.Controllers
+{
+   [Authorize]
+    public class SerCentersController : Controller
+    {
+        private ServiceCenterEntities db = new ServiceCenterEntities();
+
+        // GET: SerCenters
+        public ActionResult Index()
+        {
+            return View(db.SerCenter.ToList());
+        }
+
+        // GET: SerCenters/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SerCenter serCenter = db.SerCenter.Find(id);
+            if (serCenter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(serCenter);
+        }
+
+        // GET: SerCenters/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SerCenters/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ServiceCenterID,ServiceCenterName,AppBookDate,VehicalType,VehicalModel")] SerCenter serCenter)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SerCenter.Add(serCenter);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(serCenter);
+        }
+
+        // GET: SerCenters/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SerCenter serCenter = db.SerCenter.Find(id);
+            if (serCenter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(serCenter);
+        }
+
+        // POST: SerCenters/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ServiceCenterID,ServiceCenterName,AppBookDate,VehicalType,VehicalModel")] SerCenter serCenter)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(serCenter).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(serCenter);
+        }
+
+        // GET: SerCenters/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SerCenter serCenter = db.SerCenter.Find(id);
+            if (serCenter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(serCenter);
+        }
+
+        // POST: SerCenters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SerCenter serCenter = db.SerCenter.Find(id);
+            db.SerCenter.Remove(serCenter);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
